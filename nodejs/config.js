5f911f78df486c10845aa826e7db708fdfeda03a
chuckpearce <chuck.pearce@lodestarbpm.com>
2015-03-19 21:32:03 -0400

    Create config.js

---
+++ nodejs/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	'port': 8080
+	, 'username': "admin"
+ 	, 'password': "12345"
+	, 'rain8net': {
+		'host': '192.168.0.50'
+		, 'port': '4999'
+		, 'address': '01'
+  	}
+	, 'aquaController': {
+		'host': '192.168.0.52'
+		, 'port': '4999'
+  	}
+};
