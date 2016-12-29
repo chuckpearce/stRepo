0d9da3e769649f59603b05d94f86bc4ecdcb3451
Chuck <chuck.pearce@lodestarbpm.com>
2016-12-29 10:50:19 -0500

    Update aquaControllerSwitch.js

--- devices/aquaControllerSwitch.js
+++ devices/aquaControllerSwitch.js
@@ -23,11 +23,11 @@ metadata {
 		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
 			state("off", label: 'Off', action: "switch.on", icon: "st.Outdoor.outdoor12-icn", backgroundColor: "#ffffff", nextState: "on")
 			state("on", label: 'On', action: "switch.off", icon: "st.Outdoor.outdoor12-icn", backgroundColor: "#79b821", nextState: "off")
-			state("offline", label: 'Offline', icon: "st.Outdoor.outdoor12-icn", backgroundColor: "#ff0000")
+			state("offline", label: 'Offline', icon: "st.Outdoor.outdoor12-icn", backgroundColor: "#ff0000", nextState: "off")
 		}
 		standardTile("modeSwitch", "device.modeSwitch", canChangeIcon: true) {
-			state("manual", label: 'Manual', icon: "st.switches.switch.off", backgroundColor: "#e6e035", nextState: "auto")
-			state("auto", label: 'Auto', action: "auto", icon: "st.switches.switch.on", backgroundColor: "#79b821")
+			state("manual", label: 'Manual', action: "auto", icon: "st.switches.switch.off", backgroundColor: "#e6e035", nextState: "auto")
+			state("auto", label: 'Auto', icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "manual")
 		}
 		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat") {
 			state("default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh")
@@ -41,19 +41,23 @@ def parse(String description) { }
 
 def on() {
 	updateDeviceStatus(1)
+	updateDeviceStatus(4)
 	parent.toggleSwitch(this, "on")
 }
 def off() {
 	updateDeviceStatus(0)
+	updateDeviceStatus(4)
 	parent.toggleSwitch(this, "off")
 }
 
 def auto() {
 	updateDeviceStatus(3)
 	parent.toggleSwitch(this, "auto")
+    refresh()
 }
 
 def refresh() {
+	log.debug('Go Refresh')
 	parent.getStatus()
 }
 
@@ -62,6 +66,7 @@ def poll() {
 }
 
 def updateDeviceStatus(status) {
+	log.debug "Calling status $status"
 	if (status == 0) { 
 		sendEvent(name: "switch", value: "off", display: true, descriptionText: device.displayName + " is off") 
 	}   
